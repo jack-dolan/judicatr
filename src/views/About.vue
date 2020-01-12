71b72a16bb55fa4136e2ee3bb5392cfe4612db28
Jack Dolan <dolan.jack.h@gmail.com>
2020-01-11 17:03:21 -0800

    finished basic RFC list pulled from Firestore

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,12 +3,14 @@
     <h1>This is an about page</h1>
     <ol>
       <li>Test</li>
-      
+      <li v-for="project in projects" v-bind:key="project.id">
+        {{ project.id }}
+      </li>
     </ol>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { db } from '../components/db'
 
 export default {
